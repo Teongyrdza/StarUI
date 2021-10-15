4642452ef3c25b73d0504af885ac3e6e846868f8
Ostap <ostap.hnatenko@gmail.com>
2021-10-15 19:30:08 +0300

    Add enum-like factory method for RoundedCornersButtonStyle.

--- Sources/StarUI/Styles/RoundedCornersButtonStyle.swift
+++ Sources/StarUI/Styles/RoundedCornersButtonStyle.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 @available(macOS 10.15, iOS 13, watchOS 6, tvOS 13, *)
 public struct RoundedCornersButtonStyle: ButtonStyle {
+    public static let defaultLineWidth: CGFloat = 10
+    public static let defaultCornerRadius: CGFloat = 5
+    
     public let lineWidth: CGFloat
     public let cornerRadius: CGFloat
     
@@ -22,9 +25,19 @@ public struct RoundedCornersButtonStyle: ButtonStyle {
         .foregroundColor(.accentColor)
     }
     
-    public init(lineWidth: CGFloat = 10, cornerRadius: CGFloat = 5) {
+    public init(lineWidth: CGFloat = defaultLineWidth, cornerRadius: CGFloat = defaultCornerRadius) {
         self.lineWidth = lineWidth
         self.cornerRadius = cornerRadius
+    }
+}
+
+@available(macOS 10.15, iOS 13, watchOS 6, tvOS 13, *)
+public extension ButtonStyle where Self == RoundedCornersButtonStyle {
+    static func roundedCorners(
+        lineWidth: CGFloat = Self.defaultLineWidth,
+        cornerRadius: CGFloat = Self.defaultCornerRadius
+    ) -> Self {
+        .init(lineWidth: lineWidth, cornerRadius: cornerRadius)
     }
 }
 
@@ -33,7 +46,7 @@ public struct RoundedCornersButtonStyle: ButtonStyle {
 struct RoundedButtonStyle_Previews: PreviewProvider {
     static var previews: some View {
         Button("Press Me", action: {})
-            .buttonStyle(RoundedCornersButtonStyle(lineWidth: 5, cornerRadius: 10))
+            .buttonStyle(.roundedCorners(lineWidth: 5, cornerRadius: 10))
             .accentColor(.purple)
             .frame(width: 75, height: 50)
     }
