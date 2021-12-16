f5453eeeab8da8578d34d04ed9b2309e93ea6623
Ostap <ostap.hnatenko@gmail.com>
2021-12-16 17:21:30 +0200

    Fix a bug when buttons with rounded corners took all available space

--- Sources/StarUI/Styles/RoundedCornersButtonStyle.swift
+++ Sources/StarUI/Styles/RoundedCornersButtonStyle.swift
@@ -8,6 +8,42 @@
 import SwiftUI
 
 @available(macOS 10.15, iOS 13, watchOS 6, tvOS 13, *)
+struct BoundsPreferenceKey: PreferenceKey {
+    typealias Value = Anchor<CGRect>?
+    
+    static let defaultValue: Value = nil
+    
+    static func reduce(value: inout Anchor<CGRect>?, nextValue: () -> Anchor<CGRect>?) {
+        value = nextValue()
+    }
+}
+
+@available(macOS 10.15, iOS 13, watchOS 6, tvOS 13, *)
+struct RoundedButton: View {
+    let lineWidth: CGFloat
+    let cornerRadius: CGFloat
+    let configuration: ButtonStyleConfiguration
+    
+    var body: some View {
+            configuration.label
+                .anchorPreference(key: BoundsPreferenceKey.self, value: .bounds) {
+                    $0
+                }
+                .backgroundPreferenceValue(BoundsPreferenceKey.self) { bounds in
+                    GeometryReader { geo in
+                        let label = geo[bounds!]
+                        
+                        RoundedRectangle(cornerRadius: cornerRadius)
+                            .stroke(lineWidth: lineWidth)
+                            .position(x: label.midX, y: label.midY)
+                            .frame(width: label.width + 20, height: label.height + 20)
+                    }
+                }
+                .foregroundColor(.accentColor)
+    }
+}
+
+@available(macOS 10.15, iOS 13, watchOS 6, tvOS 13, *)
 public struct RoundedCornersButtonStyle: ButtonStyle {
     public static let defaultLineWidth: CGFloat = 10
     public static let defaultCornerRadius: CGFloat = 5
@@ -16,13 +52,7 @@ public struct RoundedCornersButtonStyle: ButtonStyle {
     public let cornerRadius: CGFloat
     
     public func makeBody(configuration: Configuration) -> some View {
-        ZStack {
-            configuration.label
-            
-            RoundedRectangle(cornerRadius: cornerRadius)
-                .stroke(lineWidth: lineWidth)
-        }
-        .foregroundColor(.accentColor)
+        RoundedButton(lineWidth: lineWidth, cornerRadius: cornerRadius, configuration: configuration)
     }
     
     public init(lineWidth: CGFloat = defaultLineWidth, cornerRadius: CGFloat = defaultCornerRadius) {
@@ -49,10 +79,16 @@ public extension ButtonStyle where Self == RoundedCornersButtonStyle {
 @available(macOS 11, iOS 13, watchOS 6, tvOS 13, *)
 struct RoundedButtonStyle_Previews: PreviewProvider {
     static var previews: some View {
-        Button("Press Me", action: {})
-            .buttonStyle(.roundedCorners(lineWidth: 5, cornerRadius: 10))
-            .accentColor(.purple)
-            .frame(width: 75, height: 50)
+        VStack {
+            Button("Press Me", action: {})
+                .buttonStyle(.roundedCorners(lineWidth: 5, cornerRadius: 10))
+                .accentColor(.purple)
+                .padding(.bottom, 50)
+            
+            Button("No, me!", action: {})
+                .buttonStyle(.roundedCorners)
+                .accentColor(.yellow)
+        }
     }
 }
 #endif
